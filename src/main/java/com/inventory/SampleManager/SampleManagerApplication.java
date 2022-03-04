7337ab02a89c80d0549bd237bb0bc827c9fa76e3
Andy Martin <anjemart1991@gmail.com>
2022-03-04 14:14:49 -0500

    initial commit

---
+++ src/main/java/com/inventory/SampleManager/SampleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.SampleManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventory/SampleManager/SampleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.SampleManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
