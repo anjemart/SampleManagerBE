be4ac104987439a523b82005d89b32b40f1115ff
anjemart-gg <76923168+anjemart-gg@users.noreply.github.com>
2022-03-25 14:47:56 -0400

    adding foreign keys, updating casing

--- src/main/resources/db/migration/V1__Test.sql
+++ src/main/resources/db/migration/V1__Test.sql
@@ -1,25 +1,33 @@
-create sequence if not exists hibernate_sequence;
+CREATE SEQUENCE IF NOT EXISTS hibernate_sequence;
 
-create table if not exists sample(
-    id int GENERATED ALWAYS AS IDENTITY,
-    external_id varchar(100) NOT NULL,
-    name varchar(100) NOT NULL
+CREATE TABLE IF NOT EXISTS sample(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    external_id VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
 );
 
-create table if not exists customer(
-    id int GENERATED ALWAYS AS IDENTITY,
-    first_name varchar(100) NOT NULL,
-    last_name varchar(100) NOT NULL,
-    home_phone varchar(20),
-    cell_phone varchar(20),
-    email varchar(100)
+CREATE TABLE IF NOT EXISTS customer(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    home_phone VARCHAR(20),
+    cell_phone VARCHAR(20),
+    email VARCHAR(100),
+    PRIMARY KEY(id)
 );
 
-create table if not exists checked_out_sample(
-    id int GENERATED ALWAYS AS IDENTITY,
-    sample_id int,
-    customer_id int,
-    followed_up boolean,
-    created_on timestamptz Default now(),
-    last_modified_on timestamptz Default now()
+CREATE TABLE IF NOT EXISTS checked_out_sample(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    sample_id INT,
+    customer_id INT,
+    followed_up BOOLEAN,
+    created_on TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    last_modified_on TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_CUSTOMER
+      FOREIGN KEY(customer_id)
+        REFERENCES customer(id),
+    CONSTRAINT FK_SAMPLE
+          FOREIGN KEY(sample_id)
+            REFERENCES sample(id)
 );
